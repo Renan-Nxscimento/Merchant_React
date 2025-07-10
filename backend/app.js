1f841194dc12f58bdb9041bf04f15492587eef7c
Renan Nascimento <146897996+Renan-Nxscimento@users.noreply.github.com>
2025-07-10 02:55:10 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,6 @@ const routes = require('./routes/router')
 
 app.use('/api', routes)
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log(`Commencing merchant application.`)
 })
